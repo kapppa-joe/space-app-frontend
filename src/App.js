017ca75286890ccf06a9cf8d04e9a3398138fbcb
Rachel <rachelanna92@outlook.com>
2021-11-02 10:43:54 +0000

    add background component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
+import Background from "./components/Background";
 import UserAuth from "./components/UserAuth.jsx";
 import Onboarding from "./components/Onboarding";
 import MissionControl from "./components/MissionControl";
 import SolarSystem from "./components/SolarSystem";
+import Planets from "./components/Planets";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <UserAuth />
-      </Route>
-      <Route exact path="/onboarding">
-        <Onboarding />
-      </Route>
-      <Route exact path="/mission-control">
-        <MissionControl />
-      </Route>
-      <Route exact path="/solar-system">
-        <SolarSystem />
-      </Route>
-      <Route exact path="/planets/:planet_id">
-        <SolarSystem />
-      </Route>
-    </Switch>
+    <Background>
+      <Switch>
+        <Route exact path="/">
+          <UserAuth />
+        </Route>
+        <Route exact path="/onboarding">
+          <Onboarding />
+        </Route>
+        <Route exact path="/mission-control">
+          <MissionControl />
+        </Route>
+        <Route exact path="/solar-system">
+          <SolarSystem />
+        </Route>
+        <Route exact path="/space/:planet_id">
+          <Planets />
+        </Route>
+      </Switch>
+    </Background>
   );
 }
 
