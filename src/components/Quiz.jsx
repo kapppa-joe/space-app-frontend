61356866d46e1281277509f05d07e4266488752f
Rachel <rachelanna92@outlook.com>
2021-11-04 17:00:45 +0000

    remove useParams

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import {
   getQuestions,
   updateUserProgress,
