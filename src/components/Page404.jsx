534c9424c8d17554dd3538d755902f2e8c59e2ef
Dan <danielkbeale@gmail.com>
2021-11-08 12:46:06 +0000

    added 404 message and image

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Page404 = () => {
     return (
-        <div>
-            <p>404: Turn Back All Ye Who Enter Here!</p>
+        <div className="section">
+            <h2 className="title is-size-2 has-text-white">404: Houston, we have a problem!</h2>
+            <img alt="missing astronaut" src="/assets/2d-images/lost_in_space.png"/>
         </div>
     );
 };
