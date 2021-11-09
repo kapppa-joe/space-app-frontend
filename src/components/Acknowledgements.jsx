271d1d7cc81308cf130984699576c4799f8e30d4
Joe Fong <kapppa.joe@gmail.com>
2021-11-09 16:53:34 +0000

    add styles to acknowledgements page

--- src/components/Acknowledgements.jsx
+++ src/components/Acknowledgements.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 const Acknowledgements = () => {
   return (
-    <div>
-      <p>We would like to thank the following for making this app possible:</p>
-      <ul>
-        <li>
-          <a href="https://solarsystem.nasa.gov/resources/">
-            Nasa from providing us with factual information and 3D models.
-          </a>
-          <a href="https://www.freepik.com">
-            freepik for providing us with images and icons.
-          </a>
-        </li>
-      </ul>
+    <div className="acknowledgements">
+      <div className="message-card">
+        <p>
+          We would like to thank the following for making this app possible:
+        </p>
+        <ul>
+          <li>
+            <a href="https://solarsystem.nasa.gov/resources/">
+              Nasa from providing us with factual information and 3D models.
+            </a>
+            <br />
+            <a href="https://www.freepik.com">
+              Freepik for providing us with images and icons.
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
