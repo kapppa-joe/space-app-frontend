033c6bb3a6f934474b699a4a5a01dc166d9ed179
Rachel <rachelanna92@outlook.com>
2021-11-09 14:35:59 +0000

    style login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -53,15 +53,15 @@ export default function UserAuth() {
   }
   if (loading) return <Loading />;
   return (
-    <div>
       <div className="Login">
         <h1 className="main-heading">Out of Orbit</h1>
         <h4 id="tagline">An augmented reality space experience</h4>
-        <p>Login or register to begin your voyage into outer space</p>
+        <section className="login-container">
+        <p id="login-form-text">Login or register to begin your voyage into outer space</p>
         <form>
           <div className="form-grid">
             <label id="email" htmlFor="email">
-              E-mail
+              E-mail:
             </label>
             <input
               id="email-input"
@@ -71,7 +71,7 @@ export default function UserAuth() {
             />
             <br />
             <label id="password" htmlFor="password">
-              Password
+              Password:
             </label>
             <input
               id="password-input"
@@ -81,14 +81,17 @@ export default function UserAuth() {
             />
             <br />
           </div>
-          <button className="login_signInButton" onClick={signIn} type="submit">
-            Sign In
+          <button className="button" onClick={signIn} type="submit">
+          <svg id="login-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="right-to-bracket" class="svg-inline--fa fa-right-to-bracket" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M344.7 238.5l-144.1-136C193.7 95.97 183.4 94.17 174.6 97.95C165.8 101.8 160.1 110.4 160.1 120V192H32.02C14.33 192 0 206.3 0 224v64c0 17.68 14.33 32 32.02 32h128.1v72c0 9.578 5.707 18.25 14.51 22.05c8.803 3.781 19.03 1.984 26-4.594l144.1-136C354.3 264.4 354.3 247.6 344.7 238.5zM416 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c17.67 0 32 14.33 32 32v256c0 17.67-14.33 32-32 32h-64c-17.67 0-32 14.33-32 32s14.33 32 32 32h64c53.02 0 96-42.98 96-96V128C512 74.98 469 32 416 32z"></path></svg>
+          <p className="button-text">Login</p>
           </button>
-          <button className="login_registerButton" onClick={register}>
-            Create your account
+          <br/>
+          <button className="button" onClick={register}>
+          <svg id="register-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-plus" class="svg-inline--fa fa-user-plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.31 128-128S294.7 0 224 0C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3C0 496.5 15.52 512 34.66 512h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304zM616 200h-48v-48C568 138.8 557.3 128 544 128s-24 10.75-24 24v48h-48C458.8 200 448 210.8 448 224s10.75 24 24 24h48v48C520 309.3 530.8 320 544 320s24-10.75 24-24v-48h48C629.3 248 640 237.3 640 224S629.3 200 616 200z"></path></svg>
+            <p className="button-text">Create a new account</p>
           </button>
         </form>
+        </section>
       </div>
-    </div>
   );
 }
