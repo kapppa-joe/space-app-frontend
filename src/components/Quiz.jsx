9a15fd207a8e97bc9028910700e4f41a491cba5c
Joe Fong <kapppa.joe@gmail.com>
2021-11-09 11:40:40 +0000

    add styling to quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -11,6 +11,46 @@ import QuizModal from "./QuizModal";
 import FinishedQuizModal from "./FinishedQuizModal";
 import Loading from "./Loading";
 
+const CheckMark = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fas"
+      data-icon="circle-check"
+      class="icon check-mark svg-inline--fa fa-circle-check"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="currentColor"
+        d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM371.8 211.8l-128 128C238.3 345.3 231.2 348 224 348s-14.34-2.719-19.81-8.188l-64-64c-10.91-10.94-10.91-28.69 0-39.63c10.94-10.94 28.69-10.94 39.63 0L224 280.4l108.2-108.2c10.94-10.94 28.69-10.94 39.63 0C382.7 183.1 382.7 200.9 371.8 211.8z"
+      ></path>
+    </svg>
+  );
+};
+
+const XMark = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fas"
+      data-icon="circle-xmark"
+      className="icon x-mark svg-inline--fa fa-circle-xmark"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="currentColor"
+        d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256C397.4 512 512 397.4 512 256S397.4 0 256 0zM336.1 303c9.375 9.375 9.375 24.56 0 33.94c-9.381 9.381-24.56 9.373-33.94 0L256 289.9l-47.03 47.03c-9.381 9.381-24.56 9.373-33.94 0c-9.375-9.375-9.375-24.56 0-33.94l47.03-47.03L175 208.1c-9.375-9.375-9.375-24.56 0-33.94s24.56-9.375 33.94 0L256 222.1l47.03-47.03c9.375-9.375 24.56-9.375 33.94 0s9.375 24.56 0 33.94l-47.03 47.03L336.1 303z"
+      ></path>
+    </svg>
+  );
+};
+
 const Quiz = ({ space_object }) => {
   const [user, loading, error] = useAuthState(auth);
   const [quiz, setQuiz] = useState(null);
@@ -18,7 +58,7 @@ const Quiz = ({ space_object }) => {
   const [progress, setProgress] = useState([]);
   const [incorrect, setIncorrect] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(1);
-  const [contentLoading, setContentLoading] = useState(true); 
+  const [contentLoading, setContentLoading] = useState(true);
   const [hasWonBadge, setHasWonBadge] = useState(false);
   const [openWonBadgeModal, setOpenWonBadgeModal] = useState(false);
 
@@ -47,7 +87,7 @@ const Quiz = ({ space_object }) => {
       })
       .finally(() => {
         setContentLoading(false);
-      })
+      });
   }, [space_object]);
 
   const checkAnswer = (e) => {
@@ -81,7 +121,8 @@ const Quiz = ({ space_object }) => {
     return (
       <div>
         <p>
-          Sorry we cant find any questions at the minute. Please try again later.
+          Sorry we cant find any questions at the minute. Please try again
+          later.
         </p>
       </div>
     );
@@ -90,18 +131,16 @@ const Quiz = ({ space_object }) => {
   if (!user && !loading) {
     return <Redirect to="/" />;
   } else if (contentLoading) {
-    return (
-      <Loading />
-    )
+    return <Loading />;
   } else {
     return (
-      <div id="quiz-modal">
+      <div className="quiz-modal">
         <QuizModal
           openWonBadgeModal={openWonBadgeModal}
           setOpenWonBadgeModal={setOpenWonBadgeModal}
           space_object={space_object}
         />
-        <h1>Quiz:</h1>
+        <h2 className="quiz-title">Quiz</h2>
         <section
           className={`quiz-box ${
             progress.includes(currentQuestion)
@@ -113,13 +152,14 @@ const Quiz = ({ space_object }) => {
         >
           <ul>
             <li>
-              <p id="quiz-question">
+              <p className="question">
                 Question: {quiz[currentQuestion].question}
               </p>
-              <div>
+              <div className="quiz-button-container">
                 {quiz[currentQuestion].answers.map((answer) => {
                   return (
                     <button
+                      className="button quiz-button"
                       key={answer}
                       disabled={
                         progress.includes(currentQuestion) ||
@@ -136,7 +176,9 @@ const Quiz = ({ space_object }) => {
 
               {/* <p>Correct: {quiz[currentQuestion].correct}</p> */}
             </li>
+
             <button
+              className="button quiz-button"
               onClick={() => {
                 setCurrentQuestion((curr) => curr - 1);
               }}
@@ -144,20 +186,11 @@ const Quiz = ({ space_object }) => {
             >
               Previous
             </button>
-            {progress.includes(currentQuestion) ? (
-              <p>
-                That's correct, Well done! Please move to the next question!
-              </p>
-            ) : null}
-            {incorrect.includes(currentQuestion) ? (
-              <p>
-                Unlucky, that's not the right answer. Try the next question!
-              </p>
-            ) : null}
 
             {/* hide the next question button when it is the last question */}
             {currentQuestion !== quiz.length - 1 && (
               <button
+                className="button quiz-button"
                 onClick={() => {
                   setCurrentQuestion((curr) => curr + 1);
                 }}
@@ -175,6 +208,24 @@ const Quiz = ({ space_object }) => {
                 hasWonBadge={hasWonBadge}
                 space_object={space_object}
               />
+            ) : null}
+
+            {progress.includes(currentQuestion) ? (
+              <div className="quiz-message-container">
+                <CheckMark />
+                <p className="quiz-message">
+                  That's correct, Well done! Please move on to the next
+                  question!
+                </p>
+              </div>
+            ) : null}
+            {incorrect.includes(currentQuestion) ? (
+              <div className="quiz-message-container">
+                <XMark />
+                <p className="quiz-message">
+                  Unlucky, that's not the right answer. Try the next question!
+                </p>
+              </div>
             ) : null}
           </ul>
         </section>
