bb98400eb402b71e179aa6ce86c45a81973a934c
Richard Crankshaw <ric_24@hotmail.co.uk>
2021-11-09 10:57:49 +0000

    Added SpaceStation component to facts page

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -4,11 +4,14 @@ import { getTrivia } from "../db";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 import Loading from "./Loading";
+import SpaceStation from "./SpaceStation";
+import "bulma-pageloader";
 
 const Facts = ({ space_object }) => {
   const [trivia, setTrivia] = useState(null);
   const [err, setErr] = useState(false);
   const [user, loading, error] = useAuthState(auth);
+  const [isLocationActive, setIsLocationActive] = useState(false);
 
   useEffect(() => {
     setErr(false);
@@ -25,7 +28,9 @@ const Facts = ({ space_object }) => {
     return (
       <div>
         <p>
-        <p>Sorry we can't find any facts at the minute. Please try again later. </p>
+          <p>
+            Sorry we can't find any facts at the minute. Please try again later.{" "}
+          </p>
         </p>
       </div>
     );
@@ -34,15 +39,41 @@ const Facts = ({ space_object }) => {
   if (!user && !loading) {
     return <Redirect to="/" />;
   } else if (err) {
-    return <p>Sorry we can't find any facts at the minute. Please try again later. </p>;
-  } else if (!trivia) {
     return (
-      <Loading />
+      <p>
+        Sorry we can't find any facts at the minute. Please try again later.{" "}
+      </p>
     );
+  } else if (!trivia) {
+    return <Loading />;
   } else {
     return (
       <div className="trivia_body">
         <h2>Trivia</h2>
+        {space_object === "iss" && (
+          <>
+            <button
+              className="button"
+              onClick={() => {
+                setIsLocationActive(true);
+              }}
+            >
+              Click here to see where the ISS is now!
+            </button>
+            <div
+              className={`pageloader ${isLocationActive ? "is-active" : ""}`}
+            >
+              <SpaceStation />
+              <button
+                className="modal-close is-large"
+                aria-label="close"
+                onClick={() => {
+                  setIsLocationActive(false);
+                }}
+              ></button>
+            </div>
+          </>
+        )}
         <ul className="trivia_list">
           {trivia.map((fact, index) => {
             return (
