c6778ad096f1e765eb9329a0ca9f1b2318e38280
Rachel <rachelanna92@outlook.com>
2021-11-09 22:21:12 +0000

    implement helper function to format space object names

---
+++ src/utils/helperFunctions.js
@@ -0,0 +1,20 @@
+export const formatSpaceObject = (spaceObject) => {
+  const splitChars = spaceObject.split("");
+
+  const whiteSpaceObject = splitChars.map((char) => {
+    if (char === "_" || char === "-") {
+      return " ";
+    }
+    return char;
+  });
+
+  const splitWords = whiteSpaceObject.join("").split(" ");
+
+  const upperCaseWords = splitWords.map((word) => {
+    const firstLetter = word[0].toUpperCase();
+    const remainingLetters = word.substring(1);
+    return firstLetter + remainingLetters;
+  });
+
+  return upperCaseWords.join(" ");
+};
