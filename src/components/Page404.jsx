fba0df1bfb9bc97381de47b81fe133e98211bdbd
Rachel <rachelanna92@outlook.com>
2021-11-09 22:34:44 +0000

    change font and alter image size

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Page404 = () => {
     return (
-        <div className="section">
-            <h2 className="title is-size-2 has-text-white">404: Houston, we have a problem!</h2>
-            <img alt="missing astronaut" src="/assets/2d-images/lost_in_space.png"/>
+        <div>
+            <h5 className="title is-size-3 has-text-white">404: Houston, we have a problem!</h5>
+            <p>It looks like this page doesn't exist</p>
+            <img className="error-img" alt="missing astronaut" src="/assets/2d-images/lost_in_space.png"/>
         </div>
     );
 };
