c6c7b1265d52774e62eb131092bc658448b7f446
Rachel <rachelanna92@outlook.com>
2021-11-10 22:12:38 +0000

    style acknowledgement page

--- src/components/Acknowledgements.jsx
+++ src/components/Acknowledgements.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Acknowledgements = () => {
   return (
-    <div className="acknowledgements">
-      <div className="message-card">
+    <div className="acknowledgements ">
+      <h3>Thank You!</h3>
+      <div className="onboarding-text-container">
         <p>
           We would like to thank the following for making this app possible:
         </p>
-        <ul>
-          <li>
-            <a href="https://solarsystem.nasa.gov/resources/">
-              Nasa from providing us with factual information and 3D models.
-            </a>
-            <br />
-            <a href="https://www.freepik.com">
-              Freepik for providing us with images and icons.
-            </a>
-          </li>
-        </ul>
+        <br/>
+        <p>
+        &bull; &nbsp; <a href="https://solarsystem.nasa.gov/resources/">
+              NASA</a> for providing us with factual information and 3D models
+        </p>
+        <p>
+        &bull; &nbsp; <a href="https://www.freepik.com">
+              Freepik</a> for providing many of the graphics used across the app
+        </p>
+        </div>
+        <Link to="/mission-control">
+          <button className="button" id="acknowledgement-back-button">
+                Go Back
+          </button>
+        </Link>
       </div>
-    </div>
   );
 };
 
