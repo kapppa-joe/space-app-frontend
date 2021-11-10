5edb74baf880285e0864ee771cbded15cfd49e65
Joe Fong <kapppa.joe@gmail.com>
2021-11-10 08:56:03 +0000

    fix a bug on 404 not-found page where top margin shows white background

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Page404 = () => {
-    return (
-        <div>
-            <h5 className="title is-size-3 has-text-white">404: Houston, we have a problem!</h5>
-            <p>It looks like this page doesn't exist</p>
-            <img className="error-img" alt="missing astronaut" src="/assets/2d-images/lost_in_space.png"/>
-        </div>
-    );
+  return (
+    <div id="not-found">
+      <h2 className="title is-size-3 has-text-white">
+        404: Houston, we have a problem!
+      </h2>
+      <p>It looks like this page doesn't exist</p>
+      <img
+        className="error-img"
+        alt="missing astronaut"
+        src="/assets/2d-images/lost_in_space.png"
+      />
+    </div>
+  );
 };
 
 export default Page404;
